5c83a11b5a3611dd022d9b84deaf8fd8ce939c15
RAYAN88-tech <rayanazmat11@gmail.com>
2026-07-28 17:57:10 +0500

    Add files via upload

--- Calculator/Calculator/script.js
+++ Calculator/Calculator/script.js
@@ -5,11 +5,11 @@ function printHistory(num){
     document.getElementById("history-value").innerText=num;
 }
 function getOutput(){
-    return document.getElementById(output-value).innerText;
+    return document.getElementById("output-value").innerText;
 }
 function printOutput(num){
     if(num==""){
-        document.getElementById(output-value).innerText=num;
+        document.getElementById("output-value").innerText=num;
     }
     else{
         document.getElementById("output-value").innerText=getFormattedNumber(num);
@@ -26,7 +26,7 @@ function getFormattedNumber(num){
 function reverseNumberFormat(num){
     return Number(num.replace(/,/g,''));
 }
-var operator = document.getElementByClassName("operator");
+var operator = document.getElementsByClassName("operator");
 for(var i=0;i<operator.length;i++){
     operator[i].addEventListener('click',function(){
         if(this.id=="clear"){
